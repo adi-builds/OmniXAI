da40c709afecd26c649f4385d42f1c9c48e68345
adi-builds <adityapaul500@gmail.com>
2026-05-19 14:08:06 +0530

    completed homepage

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
+import AiTools from '../Components/AiTools'
+import Testiimonial from '../Components/Testiimonial'
+import Plan from '../Components/Plan'
+import Footer from '../Components/Footer'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <AiTools/>
+    <Testiimonial/>
+    <Plan/>
+    <Footer/>
     </>
   )
 }
