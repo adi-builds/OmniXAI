76356d9f87d1f715b1100a08901c2a0ebec93cf0
adi-builds <adityapaul500@gmail.com>
2026-05-20 17:48:01 +0530

    remove node_modules from repository

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config';
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
+
+app.get('/',(req,res)=>res.send('Hello from Omnix AI!'))
+
+app.use(requireAuth())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
